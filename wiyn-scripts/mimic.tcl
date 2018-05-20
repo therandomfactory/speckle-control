657c6a784f43005258408674088280b3a2f13170
Dave Mills <rfactory@theriver.com>
2018-05-20 16:09:35 -0700

    Update mimic animation for gemini

--- wiyn-scripts/mimic.tcl
+++ wiyn-scripts/mimic.tcl
@@ -3,58 +3,57 @@
 #
 #
 
-toplevel .mimicNessi -width 850 -height 640
+proc createMimicDiagram { baseimg } {
+global MOFF XO YO NESSI_DIR
+  catch {destroy .mimicNessi}
+  set XO [lindex $MOFF($baseimg) 0]
+  set YO [lindex $MOFF($baseimg) 1]
+  toplevel .mimicNessi -width 850 -height 640
+  canvas .mimicNessi.myCanvas  -width 849 -height 640 
+  pack .mimicNessi.myCanvas
+  set mimic [image create photo]
+  $mimic read $NESSI_DIR/wiyn-scripts/[set baseimg].gif
+  .mimicNessi.myCanvas create image 0 0 -anchor nw -image $mimic
+  set oredwide   [.mimicNessi.myCanvas create oval 50 50 80 100 -fill yellow]
+  set obluewide  [.mimicNessi.myCanvas create oval 50 50 100 80 -fill yellow]
+  set oredspck   [.mimicNessi.myCanvas create oval 50 50 40 60 -fill yellow]
+  set obluespck  [.mimicNessi.myCanvas create oval 50 50 60 40 -fill yellow]
+  set oinpwide   [.mimicNessi.myCanvas create oval 50 50 80 140 -fill yellow]
+  set oinpspck   [.mimicNessi.myCanvas create oval 50 50 80 100 -fill yellow]
+  set oredshut   [.mimicNessi.myCanvas create oval 50 50 40 80 -fill white]
+  set oblueshut  [.mimicNessi.myCanvas create oval 50 50 80 40 -fill white]
+  label .mimicNessi.myCanvas.redfilt -width 10  -text "clear" 
+  place .mimicNessi.myCanvas.redfilt -x [expr $XO+410] -y  [expr $YO+80]
+  label .mimicNessi.myCanvas.bluefilt  -width 10  -text "clear" 
+  place .mimicNessi.myCanvas.bluefilt -x [expr $XO+306] -y  [expr $YO+220]
+  label .mimicNessi.myCanvas.redroi  -width 10  -text "1024x1024" 
+  place .mimicNessi.myCanvas.redroi -x [expr $XO+716] -y  [expr $YO+120]
+  label .mimicNessi.myCanvas.blueroi  -width 10  -text "1024x1024" 
+  place .mimicNessi.myCanvas.blueroi -x [expr $XO+266] -y [expr $YO+540]
+  label .mimicNessi.myCanvas.redtemp  -width 10  -text "0.0 deg" 
+  place .mimicNessi.myCanvas.redtemp -x [expr $XO+716] -y  [expr $YO+140]
+  label .mimicNessi.myCanvas.bluetemp  -width 10  -text "0.0 deg" 
+  place .mimicNessi.myCanvas.bluetemp -x [expr $XO+266] -y  [expr $YO+560]
+}
 
-canvas .mimicNessi.myCanvas  -width 849 -height 640 
-pack .mimicNessi.myCanvas
-set mimic [image create photo]
-$mimic read $NESSI_DIR/wiyn-scripts/mimic.gif
-.mimicNessi.myCanvas create image 0 0 -anchor nw -image $mimic
-
-set oredwide   [.mimicNessi.myCanvas create oval 50 50 80 100 -fill yellow]
-set obluewide  [.mimicNessi.myCanvas create oval 50 50 100 80 -fill yellow]
-
-set oredspck   [.mimicNessi.myCanvas create oval 50 50 40 60 -fill yellow]
-set obluespck  [.mimicNessi.myCanvas create oval 50 50 60 40 -fill yellow]
-
-set oinpwide  [.mimicNessi.myCanvas create oval 50 50 80 140 -fill yellow]
-set oinpspck  [.mimicNessi.myCanvas create oval 50 50 80 100 -fill yellow]
-
-set oredshut  [.mimicNessi.myCanvas create oval 50 50 40 80 -fill white]
-set oblueshut [.mimicNessi.myCanvas create oval 50 50 80 40 -fill white]
-
-label .mimicNessi.myCanvas.redfilt -width 10  -text "clear" 
-place .mimicNessi.myCanvas.redfilt -x 410 -y 80
-
-label .mimicNessi.myCanvas.bluefilt  -width 10  -text "clear" 
-place .mimicNessi.myCanvas.bluefilt -x 306 -y 220
-
-label .mimicNessi.myCanvas.redroi  -width 10  -text "1024x1024" 
-place .mimicNessi.myCanvas.redroi -x 716 -y 120
-label .mimicNessi.myCanvas.blueroi  -width 10  -text "1024x1024" 
-place .mimicNessi.myCanvas.blueroi -x 266 -y 540
-
-label .mimicNessi.myCanvas.redtemp  -width 10  -text "0.0 deg" 
-place .mimicNessi.myCanvas.redtemp -x 716 -y 140
-label .mimicNessi.myCanvas.bluetemp  -width 10  -text "0.0 deg" 
-place .mimicNessi.myCanvas.bluetemp -x 266 -y 560
 
 proc mimicMode { arm mode {value ""} } {
-global oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut oblueshut
+global oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut oblueshut XO YO MIMIC
+   set MIMIC($arm,$mode) "$value"
    if { $arm == "red" } {
      if { $mode == "wide" } {
-       .mimicNessi.myCanvas moveto $oredwide 450 115
-       .mimicNessi.myCanvas moveto $oredspck 460 180
+       .mimicNessi.myCanvas moveto $oredwide [expr $XO+450] [expr $YO+115]
+       .mimicNessi.myCanvas moveto $oredspck [expr $XO+460 ] [expr $YO+180]
      }
      if { $mode == "speckle" } {
-       .mimicNessi.myCanvas moveto $oredwide 450 60
-       .mimicNessi.myCanvas moveto $oredspck 460 135
+       .mimicNessi.myCanvas moveto $oredwide [expr $XO+450 ] [expr $YO+60]
+       .mimicNessi.myCanvas moveto $oredspck [expr $XO+460 ] [expr $YO+135]
      }
      if { $mode == "open" } {
-       .mimicNessi.myCanvas moveto $oredshut 670 124
+       .mimicNessi.myCanvas moveto $oredshut [expr $XO+670 ] [expr $YO+124]
      }
      if { $mode == "close" } {
-       .mimicNessi.myCanvas moveto $oredshut 1000 124
+       .mimicNessi.myCanvas moveto $oredshut [expr $XO+1000 ] [expr $YO+124]
      }
      if { $mode == "filter" } {
        .mimicNessi.myCanvas.redfilt configure -text $value
@@ -68,18 +67,18 @@ global oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut obluesh
    }
    if { $arm == "blue" } {
      if { $mode == "wide" } {
-       .mimicNessi.myCanvas moveto $obluewide 276 260
-       .mimicNessi.myCanvas moveto $obluespck 341 268 
+       .mimicNessi.myCanvas moveto $obluewide [expr $XO+276 ] [expr $YO+260]
+       .mimicNessi.myCanvas moveto $obluespck [expr $XO+341 ] [expr $YO+268]
      }
      if { $mode == "speckle" } {
-       .mimicNessi.myCanvas moveto $obluewide 221 260
-       .mimicNessi.myCanvas moveto $obluespck 297 268
+       .mimicNessi.myCanvas moveto $obluewide [expr $XO+221 ] [expr $YO+260]
+       .mimicNessi.myCanvas moveto $obluespck [expr $XO+297 ] [expr $YO+268]
      }
      if { $mode == "open" } {
-       .mimicNessi.myCanvas moveto $oblueshut 286 468
+       .mimicNessi.myCanvas moveto $oblueshut [expr $XO+286 ] [expr $YO+468]
      }
      if { $mode == "close" } {
-       .mimicNessi.myCanvas moveto $oblueshut 286 1000
+       .mimicNessi.myCanvas moveto $oblueshut [expr $XO+286 ] [expr $YO+1000]
      }
      if { $mode == "filter" } {
        .mimicNessi.myCanvas.bluefilt configure -text $value
@@ -93,15 +92,39 @@ global oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut obluesh
   }
    if { $arm == "input" } {
      if { $mode == "wide" } {
-       .mimicNessi.myCanvas moveto $oinpwide 200 95
-       .mimicNessi.myCanvas moveto $oinpspck 200 35
+       .mimicNessi.myCanvas moveto $oinpwide [expr $XO+200 ] [expr $YO+95]
+       .mimicNessi.myCanvas moveto $oinpspck [expr $XO+200 ] [expr $YO+35]
      }
      if { $mode == "speckle" } {
-       .mimicNessi.myCanvas moveto $oinpwide 200 180
-       .mimicNessi.myCanvas moveto $oinpspck 200 115
+       .mimicNessi.myCanvas moveto $oinpwide [expr $XO+200 ] [expr $YO+180]
+       .mimicNessi.myCanvas moveto $oinpspck [expr $XO+200 ] [expr $YO+115]
      }
    }
 }
+
+proc mimicOffset { x y } {
+global oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut oblueshut
+   foreach w "oredwide obluewide  oredspck obluespck oinpwide oinpspck oredshut oblueshut" {
+       .mimicNessi.myCanvas move [set [set w]] $x $y
+   }
+}
+
+proc updateMimic { } {
+global MIMIC
+  foreach w [array names MIMIC] {
+     set arm [lindex [split $w ,] 0]
+     set mode [lindex [split $w ,] 1]
+     set value $MIMIC($arm,$mode)
+     mimicMode $arm $mode $value
+  }
+}
+
+
+set MOFF(mimic-picoin) "-29 7"
+set MOFF(mimic) 	"0 0"
+set MOFF(mimic-picoout) "-46 19"
+createMimicDiagram mimic
+
 
 mimicMode red wide
 mimicMode blue wide
