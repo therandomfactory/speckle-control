dfc6f58a9bc32c434d38b25bc266890286b7b309
Dave Mills <rfactory@theriver.com>
2018-07-03 12:15:10 -0700

    Update binning usage in wcs headers

--- gui-scripts/astrometry.tcl
+++ gui-scripts/astrometry.tcl
@@ -6,15 +6,15 @@ global SCOPE ACQREGION PSCALES ANDOR_CFG PI DS9 ANDOR_ARM
   set fout [open /tmp/[set ANDOR_ARM]wcs.wcs w]
   puts $fout "CRVAL1 $decdeg"
   puts $fout "CRVAL2 $radeg"
-  puts $fout "CRPIX1 [expr $ACQREGION(geom)/$ACQREGION(bin)/2]"
-  puts $fout "CRPIX2 [expr $ACQREGION(geom)/$ACQREGION(bin)/2]"
+  puts $fout "CRPIX1 [expr $ACQREGION(geom)/$ANDOR_CFG(binning)/2]"
+  puts $fout "CRPIX2 [expr $ACQREGION(geom)/$ANDOR_CFG(binning)/2]"
   set fac 1.0
   if { $ANDOR_ARM == "blue" } {set fac -1.0}
-  puts $fout "CD1_1 [expr $fac*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ACQREGION(bin)]"              
+  puts $fout "CD1_1 [expr $fac*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ANDOR_CFG(binning)]"              
      
   puts $fout "CD1_2 0.0"
   puts $fout "CD2_1 0.0"
-  puts $fout "CD2_2 [expr -1.0*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ACQREGION(bin)]"
+  puts $fout "CD2_2 [expr -1.0*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ANDOR_CFG(binning)]"
   puts $fout "CTYPE1 'DEC--TAN'"
   puts $fout "CTYPE2 'RA--TAN'" 
   puts $fout "WCSNAME 'FK5'"
@@ -33,27 +33,27 @@ global ACQREGION SCOPE PSCALES ANDOR_CFG PI ANDOR_ARM
   $fid put keyword $r
   set r [fitshdrrecord  CRVAL2	 string "$radeg"	"Declination of reference pixel \[deg\]"]
   $fid put keyword $r
-  set r [fitshdrrecord  CRPIX1	 integer [expr $ACQREGION(geom)/$ACQREGION(bin)/2]	"Coordinate reference pixel in X"]
+  set r [fitshdrrecord  CRPIX1	 integer [expr $ACQREGION(geom)/$ANDOR_CFG(binning)/2]	"Coordinate reference pixel in X"]
   $fid put keyword $r
-  set r [fitshdrrecord  CRPIX2	 integer [expr $ACQREGION(geom)/$ACQREGION(bin)/2]	"Coordinate reference pixel in Y"]
+  set r [fitshdrrecord  CRPIX2	 integer [expr $ACQREGION(geom)/$ANDOR_CFG(binning)/2]	"Coordinate reference pixel in Y"]
   $fid put keyword $r
   set fac 1.0
   if { $ANDOR_ARM == "blue" } {set fac -1.0}
-  set r [fitshdrrecord  CD1_1	 double [expr $fac*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ACQREGION(bin)]  "Coordinate scale matrix \[degrees / pixel\]"]           
+  set r [fitshdrrecord  CD1_1	 double [expr $fac*$PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ANDOR_CFG(binning)]  "Coordinate scale matrix \[degrees / pixel\]"]           
   $fid put keyword $r
   set r [fitshdrrecord  CD1_2	 double  0.0	"Coordinate scale matrix \[degrees / pixel\]"]
   $fid put keyword $r
   set r [fitshdrrecord  CD2_1	 double  0.0	"Coordinate scale matrix \[degrees / pixel\]"]
   $fid put keyword $r
-  set r [fitshdrrecord  CD2_2	 double  [expr $PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ACQREGION(bin)]	"Coordinate scale matrix \[degrees / pixel\]"]
+  set r [fitshdrrecord  CD2_2	 double  [expr $PSCALES($SCOPE(telescope),$ANDOR_CFG(frame))*$ANDOR_CFG(binning)]	"Coordinate scale matrix \[degrees / pixel\]"]
   $fid put keyword $r
-  set r [fitshdrrecord  CTYPE1	 string 'RA--TAN'	"Coordinate type"]
+  set r [fitshdrrecord  CTYPE1	 string "RA--TAN"	"Coordinate type"]
   $fid put keyword $r
-  set r [fitshdrrecord  CTYPE2	 string  'DEC--TAN'	"Coordinate type"]
+  set r [fitshdrrecord  CTYPE2	 string  "DEC--TAN"	"Coordinate type"]
   $fid put keyword $r
-  set r [fitshdrrecord  WCSNAME  string 'FK5'	"World coordinate system type"]
+  set r [fitshdrrecord  WCSNAME  string "FK5"	"World coordinate system type"]
   $fid put keyword $r
-  set r [fitshdrrecord  RADECSYS string 'FK5'	"Default coordinate system type"]
+  set r [fitshdrrecord  RADECSYS string "FK5"	"Default coordinate system type"]
   $fid put keyword $r
   set r [fitshdrrecord  EQUINOX	 float 2000.	"Default coordinate system Equinox"]
   $fid put keyword $r
@@ -85,7 +85,7 @@ set PSCALES(WIYN,speckle) 	[expr 0.0182/3600./180.*$PI]
 set PSCALES(GEMINI,fullframe) 	[expr 0.0725/3600./180.*$PI]
 set PSCALES(GEMINI,speckle)	[expr 0.0096/3600./180.*$PI]
 set ACQREGION(geom) 1024
-set ACQREGION(bin) 1
+set ANDOR_CFG(binning) 1
 set ANDOR_CFG(frame) fullframe
 
 
